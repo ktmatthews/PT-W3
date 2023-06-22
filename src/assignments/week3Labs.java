79afddece104b5929026847e0c63a4c0e34abd71
Katlin Matthews <kcurtis1991.kc@gmail.com>
2023-06-22 18:34:23 -0500

    first commit being made

---
+++ src/assignments/week3Labs.java
@@ -0,0 +1,10 @@
+package assignments;
+
+public class week3Labs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kmatthews
+ *
+ */
+module Week3 {
+}
